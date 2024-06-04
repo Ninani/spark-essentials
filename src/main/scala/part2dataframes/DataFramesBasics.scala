c0b4021f8894d435b30d7179d37427eb8e2ed037
Anna Szczepanska <anna.szczepanska.krk@gmail.com>
2024-06-04 21:03:24 +0200

    DataFrames basics - solution

--- src/main/scala/part2dataframes/DataFramesBasics.scala
+++ src/main/scala/part2dataframes/DataFramesBasics.scala
@@ -74,4 +74,40 @@ object DataFramesBasics extends App {
   val manualCarsDFWithImplicits = cars.toDF("Name", "MPG", "Cylinders", "Displacement", "HP", "Weight", "Acceleration", "Year", "CountryOrigin")
 
 
+  /**
+    * Exercise:
+    * 1) Create a manual DF describing smartphones
+    *   - make
+    *   - model
+    *   - screen dimension
+    *   - camera megapixels
+    *
+    * 2) Read another file from the data/ folder, e.g. movies.json
+    *   - print its schema
+    *   - count the number of rows, call count()
+    */
+
+  var phones = Seq(
+    ("Samsung", "Galaxy S21", 6.2, 12),
+    ("Apple", "iPhone 12", 6.1, 12),
+    ("Google", "Pixel 5", 6.0, 12),
+    ("OnePlus", "8T", 6.55, 48),
+    ("Xiaomi", "Mi 11", 6.81, 108)
+  )
+
+  val manualPhonesDf = phones.toDF("Make", "Model", "ScreenDimension", "CameraMegapixels")
+  manualPhonesDf.show()
+
+  val moviesDf = spark.read
+    .format("json")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/movies.json")
+
+  moviesDf.printSchema()
+
+  println(s"Number of rows in moviesDf: ${moviesDf.count()}")
+
+
+
+
 }
